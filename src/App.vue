9a668dd8ca98fabe27b816640ccf272ae6588986
Coby Yates <cobylyates@gmail.com>
2020-01-31 15:20:51 -0700

    Created nav
    Created the nav and changed the values to reflect the prototype. Added user image/notification icon to header.
    Tried putting in logo, however it did not look great

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,99 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
+      color="deep-purple lighten-1"
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!-- <img src="./assets/boka_logo_sm.png" alt="Boka Logo" class="mr-5 logo"> -->
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="subtitle-1 white--text"><strong>BOKA</strong><span>PROFESSIONALS</span></span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-row
+        align="end"
+        style="max-width: 120px"
+        class="d-flex align-center justify-space-between pr-7"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-badge
+        :content="messages"
+        :value="messages"
+        color="red"
+        overlap
+        dot
+      >
+        <v-icon medium class="white--text">mdi-bell-outline</v-icon>
+      </v-badge>
+      <v-avatar>
+        <img
+          src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+      </v-avatar>
+      </v-row>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container class="fill-height">
+        <v-row
+          justify="center"
+          align="center"
+        >
+          <v-col class="shrink">
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      message: 5,
+      show: false,
+      items: [
+        { icon: 'mdi-view-dashboard', text: 'Dashboard' },
+        { icon: 'mdi-account-box-multiple', text: 'Clients' },
+        { icon: 'mdi-forum', text: 'Chat' },
+        { icon: 'mdi-calendar-month', text: 'Calendar' },
+        { icon: 'mdi-check', text: 'ToDo' },
+        { icon: 'mdi-settings', text: 'Account' },
+        { icon: 'mdi-file-document-edit', text: 'Contract' },
+      ]
+    }),
+    created () {
+      // this.$vuetify.theme.dark = true
+    },
+  }
 </script>
+
+<style lang="stylus" scoped>
+</style>
